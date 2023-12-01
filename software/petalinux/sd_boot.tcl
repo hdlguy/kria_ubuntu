8ec7a03ffebcb85f430a28c8eae1c739d55e1d66
pete <peter.dudley@precisionplanting.com>
2023-12-01 17:46:57 -0500

    adding xsct script to boot from sd card

---
+++ software/petalinux/sd_boot.tcl
@@ -0,0 +1,35 @@
+    # xsct sd_boot.tcl
+    proc set_bootmode {bootmode} {
+        connect
+        targets -set -filter {name =~ "PSU"}
+        stop
+        if {$bootmode == "jtag"} {
+            puts "Switch to JTAG bootmode"
+            targets -set -nocase -filter {name =~ "PSU"}
+            stop
+            # update multiboot to ZERO
+            mwr 0xffca0010 0x0
+            # change boot mode to JTAG
+            mwr 0xff5e0200 0x0100
+            # reset
+            rst -system
+        } elseif {$bootmode == "sd"} {
+
+            puts "Switch to SD1 bootmode"
+            # update multiboot to ZERO
+            mwr 0xffca0010 0x0
+            # change boot mode to SD
+            mwr 0xff5e0200 0x5100
+            # reset
+            rst -system
+     
+            #Sometimes A53 may be held in reset catch .. so start it with "con"
+            after 2000
+            con
+
+        } else {
+            puts "Unsupported bootmode $bootmode"
+        }
+     
+        puts "Done!!"
+    }
