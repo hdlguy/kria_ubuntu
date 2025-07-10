61af928fe6af5a44de0b3a723ea0d1ced0e6b1a2
hdlguy <hdlguy@hdlguy.com>
2025-07-10 19:09:33 -0400

    cleaning out non-working full version

--- full/vitis/src/fpga.h
+++
@@ -1,6 +0,0 @@
-// define AXI registers
-#define     Nreg                16  // number of 32 bit AXI registers
-
-#define 	FPGA_VERSION		0	// currently 0x07010100
-#define		FPGA_ID				1	// currently 0xdeadbeef
-
