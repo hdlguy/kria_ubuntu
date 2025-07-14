bd65aa9bc3422838241026b497f4591af2ed4666
hdlguy <hdlguy@hdlguy.com>
2025-07-14 10:24:59 -0400

    Vitis 2025.1 standalone projects

---
+++ vitis/src/fpga.h
@@ -0,0 +1,9 @@
+// define AXI registers
+#define     Nreg                16  // number of 32 bit AXI registers
+
+#define 	FPGA_VERSION		0	// currently 0x07010100
+#define		FPGA_ID				1	// currently 0xdeadbeef
+
+#define		FPGA_RGB_LED		2
+// [2:0] = LED1 [blue, green, red]
+// [6:4] = LED2 [blue, green, red]
