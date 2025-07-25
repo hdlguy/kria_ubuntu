22ee0467acc8848e7d1066b4730e4490224b58ac
hdlguy <hdlguy@hdlguy.com>
2025-07-25 14:35:53 -0400

    hls_regfile might do enough to test in hardware

--- hls/src/hls_regfile/hls_regfile.cpp
+++ hls/src/hls_regfile/hls_regfile.cpp
@@ -1,14 +1,11 @@
 #include "ap_int.h"
 #include <ap_fixed.h>
+#include <stdint.h>
 
-ap_uint<8> hls_tpg (uint32_t* wval, rval)
+void hls_regfile ( uint32_t wval[16], uint32_t rval[16])
 {
-    static uint32_t dval[16];
+#pragma HLS INTERFACE mode=s_axilite port=wval bundle=ctl_bus
+#pragma HLS INTERFACE mode=s_axilite port=rval bundle=ctl_bus
 
-    dval = wval;
-
-    rval = dval;
-
-    return(0);
 
 }
--- hls/src/hls_tpg/hls_tpg.cpp
+++ hls/src/hls_tpg/hls_tpg.cpp
@@ -1,12 +1,33 @@
 #include "ap_int.h"
 #include <ap_fixed.h>
+#include <stdint.h>
+#include "ap_axi_sdata.h"
+#include "hls_stream.h"
 
-ap_uint<8> hls_tpg ()
+#define DWIDTH 32
+#define L 10
+
+typedef ap_axiu<DWIDTH, 1, 1, 1> line_pkt;
+
+
+void hls_tpg (ap_uint<32> wval[16], ap_uint<32> rval[16], hls::stream<line_pkt> &dout)
 {
-    static ap_uint<8> dval;
+#pragma HLS INTERFACE mode=s_axilite port=wval      bundle=control
+#pragma HLS INTERFACE mode=s_axilite port=rval      bundle=control
+#pragma HLS INTERFACE mode=s_axilite port=return    bundle=control
+#pragma HLS INTERFACE mode=axis port=dout
 
-    dval++;
+    line_pkt t_out;
 
-    return(dval);
+    for (int i=0; i<L; i++) {
+        t_out.data = i;
+        t_out.keep = -1; //Enabling all bytes
+        if (i==L-1) {
+            t_out.last = 1;
+        } else {
+            t_out.last = 0;
+        }
+    }
+    dout.write(t_out);
 
 }
