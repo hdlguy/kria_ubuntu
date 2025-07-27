3090bfd2b4130a189eded229850c7f8cc657594c
hdlguy <hdlguy@hdlguy.com>
2025-07-27 13:32:13 -0400

    hls_tpg makes frames with hight and width control

--- hls/hls_tpg/hls_tpg.cpp
+++ hls/hls_tpg/hls_tpg.cpp
@@ -5,29 +5,43 @@
 #include "hls_stream.h"
 
 #define DWIDTH 32
-#define L 10
 
 typedef ap_axiu<DWIDTH, 1, 1, 1> line_pkt;
 
 
-void hls_tpg (ap_uint<32> wval[16], ap_uint<32> rval[16], hls::stream<line_pkt> &dout)
+void hls_tpg (
+    ap_uint<32> height,     // number of lines per video frame
+    ap_uint<32> width,      // number of pixels per line of video
+    hls::stream<line_pkt> &dout)
 {
-#pragma HLS INTERFACE mode=s_axilite port=wval      bundle=control
-#pragma HLS INTERFACE mode=s_axilite port=rval      bundle=control
+#pragma HLS INTERFACE mode=s_axilite port=height      bundle=control
+#pragma HLS INTERFACE mode=s_axilite port=width      bundle=control
 #pragma HLS INTERFACE mode=s_axilite port=return    bundle=control
 #pragma HLS INTERFACE mode=axis port=dout
 
     line_pkt t_out;
 
-    for (int i=0; i<L; i++) {
-        t_out.data = i;
-        t_out.keep = -1; //Enabling all bytes
-        if (i==(L-1)) {
-            t_out.last = 1;
-        } else {
-            t_out.last = 0;
+    for (int j=0; j<height; j++) {
+        for (int i=0; i<width; i++) {
+
+            t_out.data = i;
+            t_out.keep = -1; //Enabling all bytes
+
+            // indicate start of frame
+            if ((i==0) && (j==0)) {
+                t_out.user = 1;
+            } else {
+                t_out.user = 0;
+            }
+            // indicate in of line
+            if (i==(width-1)) {
+                t_out.last = 1;
+            } else {
+                t_out.last = 0;
+            }
+
+            dout.write(t_out);
         }
-        dout.write(t_out);
     }
 
 }
